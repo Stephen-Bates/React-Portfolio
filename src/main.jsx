73fd0fbf1f92dee83bf1e7410fd5f73383d8615d
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-02 21:24:13 -0400

    created pages for the site and imported them into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App'
+import About from './pages/About'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
