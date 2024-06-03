26b06b3f793e0b60f083bd854608232c1d78ac9b
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-02 21:34:40 -0400

    added ErrorPage and defined the router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,39 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import App from './App'
-import About from './pages/About'
-import Portfolio from './pages/Portfolio'
-import Contact from './pages/Contact'
-import Resume from './pages/Resume'
+import About from './pages/AboutPage'
+import Portfolio from './pages/PortfolioPage'
+import Contact from './pages/ContactPage'
+import Resume from './pages/ResumePage'
+import Error from './pages/ErrorPage'
 
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      },
+      {
+        path: '/Contact',
+        element: <Contact />
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/Resume',
+        element: <Resume />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
