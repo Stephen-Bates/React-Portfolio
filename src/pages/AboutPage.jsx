595d171b2918f42a2244dc2c0ec84b7f1f4c06b4
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-03 12:15:14 -0400

    added About page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+
+
+export default function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>
+        I'm a Fullstack developer who has been studying computers since early highschool.
+        In my early years, I would spend my free time indipendantly developing video games.
+        As I grew older, I became very interested in manufacturing and spent several years working in factories, learning everything I could about the trade.
+        Now, I find myself pulled back to where I always felt most comfortable, in the computer sciences.
+        I wonder, what new technologies I'll learn about next?
+        </p>
+      </div>
+    );
+  }
+  
