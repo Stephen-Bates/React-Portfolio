ffbf64db552bc3513f8311b395445080904a1b6a
Stephen Bates <stephen.michael.bates@outlook.com>
2024-06-09 22:38:12 -0400

    spellcheck

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,7 +8,7 @@ export default function About() {
             <div className="col-lg-6 mx-auto lead">
                 <p className="mb-4">
                     I'm a Fullstack developer who has been studying computers since early highschool.
-                    In my early years, I would spend my free time indipendantly developing video games.
+                    In my early years, I would spend my free time independently developing video games.
                     As I grew older, I became very interested in manufacturing and spent several years working in factories, learning everything I could about the trade.
                     Now, I find myself pulled back to where I always felt most comfortable, in the computer sciences.
 
