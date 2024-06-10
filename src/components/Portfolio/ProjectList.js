dccdf31aa35c54660ede7e7b3e028cd050797826
Stephen Bates <stephen.michael.bates@outlook.com>
2024-06-09 21:33:37 -0400

    fixed img importing for projects

--- src/components/Portfolio/ProjectList.js
+++ src/components/Portfolio/ProjectList.js
@@ -1,32 +1,38 @@
+import bookSearchSnapshot from '/src/assets/book-search-snapshot.png'
+import noteTakerSnapshot from '/src/assets/Note-Taker-snapshot.png'
+import passwordGeneratorSnapshot from '/src/assets/Password-Generator-snapshot.png'
+import sojournalSnapshot from '/src/assets/soJournal-snapshot.png'
+import textAppSnapshot from '/src/assets/text-app-snapshot.png'
+
 const ProjectList = [
     {
         title: 'SoJournal',
         description: 'A digital diary for keeping track of',
-        img: 'src/assets/soJournal-snapshot.png',
+        img: sojournalSnapshot,
         address: 'https://github.com/Stephen-Bates/soJournal'
     },
     {
         title: 'JATE',
         description: 'A PWA text editor',
-        img: 'src/assets/text-app_snapshot.png',
+        img: textAppSnapshot,
         address: 'https://github.com/Stephen-Bates/Text-App'
     },
     {
         title: 'Password Generator',
         description: 'A simple password generator applicatoin',
-        img: 'src/assets/Password-Generator-snapshot.png',
+        img: passwordGeneratorSnapshot,
         address: 'https://github.com/Stephen-Bates/Password-Generator'
     },
     {
         title: 'Note Taker',
         description: 'A simple note taking application',
-        img: 'src/assets/Note_Taker_snapshot.png',
+        img: noteTakerSnapshot,
         address: 'https://github.com/Stephen-Bates/Note-Taker'
     },
     {
         title: 'Book Search',
         description: 'This app utilizes a MERN stack to allow users to register with the site and save a list of books that they can search for',
-        img: 'src/assets/book-search-snapshot.png',
+        img: bookSearchSnapshot,
         address: 'https://github.com/Stephen-Bates/Book-Search'
     },
 ]
